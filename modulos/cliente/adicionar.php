4f371ebe644a338fd1e379b3f46fa1f0b342e33a
Your Name <youremail@yourdomain.com>
2022-06-02 21:21:59 -0300

    html

--- modulos/cliente/adicionar.php
+++ modulos/cliente/adicionar.php
@@ -1,26 +1,32 @@
 <?php
 
-$cliente = new Cliente();
-$cliente->setNome("Marcos");
-$cliente->setEndereco("alameda A");
-$cliente->setCpf("255553265");
-$cliente->setTelefone("2888999");
-$cliente->adicionar();
+if(isset($_POST["btn_salvar"])&&
+($_POST["btn_salvar"]=="Salvar")){
+    $cliente = new Cliente();
+    $cliente->setNome($_POST['input_nome']);
+    $cliente->setEndereco($_POST['input_endereco']);
+    $cliente->setCpf($_POST['input_cpf']);
+    $cliente->setTelefone($_POST['input_telefone']);
+    $cliente->adicionar();
+
+}
 
 ?>
 
-<label><h1>CADASTRO DE CLIENTES</h1></label>
-<label>Nome:</label>
-<input type='text' name='input_nome'/><br><br>
-<label>CPF:</label>
-<input type='text' name='input_cpf'/><br><br>
-<label>Endereço:</label>
-<input type='text' name='input_endereco'/><br><br>
-<label>Telefone:</label>
-<input type='text' name='input_telefone'/><br><br>
-<label>Sexo:</label>
-<input type='radio' name='input_sexo' value='feminino' /> 
-Feminino
-<input type='radio' name='input_sexo' value='masculino' /> 
-Masculino<br><br>
-<input type='submit' name='btn_salvar' value='Salvar' />
+<form method="POST" action="">
+    <label><h1>CADASTRO DE CLIENTES</h1></label>
+    <label>Nome:</label>
+    <input type='text' name='input_nome'/><br><br>
+    <label>CPF:</label>
+    <input type='text' name='input_cpf'/><br><br>
+    <label>Endereço:</label>
+    <input type='text' name='input_endereco'/><br><br>
+    <label>Telefone:</label>
+    <input type='text' name='input_telefone'/><br><br>
+    <label>Sexo:</label>
+    <input type='radio' name='input_sexo' value='feminino' /> 
+    Feminino
+    <input type='radio' name='input_sexo' value='masculino' /> 
+    Masculino<br><br>
+    <input type='submit' name='btn_salvar' value='Salvar' />
+</form>
